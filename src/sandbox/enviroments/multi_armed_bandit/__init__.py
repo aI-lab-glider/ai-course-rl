42d4a81e68a6af66ca0b9dfb6592b324e55a3916
Bohdan <qwebeck5@gmail.com>
2022-04-27 21:56:35 +0200

    Revert "Feature/multi armed bandit"

--- src/sandbox/enviroments/multi_armed_bandit/__init__.py
+++
@@ -1,3 +0,0 @@
-from sandbox.enviroments.multi_armed_bandit.env import BanditEnv
-from sandbox.enviroments.multi_armed_bandit.policy import EpsilonGreedy, UCB, ThompsonSampling
-from sandbox.enviroments.multi_armed_bandit.trainer import BanditTrainer
