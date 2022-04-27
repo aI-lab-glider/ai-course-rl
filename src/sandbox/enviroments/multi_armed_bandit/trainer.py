24a3615e56c297117cebf340d79bd9f5efe54002
czyjtu <maciejczyjtu@gmail.com>
2022-04-27 19:38:28 +0200

    Small refactoring of the trainer class

--- src/sandbox/enviroments/multi_armed_bandit/trainer.py
+++ src/sandbox/enviroments/multi_armed_bandit/trainer.py
@@ -1,4 +1,5 @@
 from sandbox.enviroments.multi_armed_bandit.env import BanditEnv
+from sandbox.enviroments.multi_armed_bandit.history import History
 from sandbox.enviroments.multi_armed_bandit.policy import BanditPolicy
 import matplotlib.pyplot as plt
 from typing import Union
@@ -9,9 +10,7 @@ class BanditTrainer:
     def __init__(self, env: BanditEnv, policy: Union[BanditPolicy, list[BanditPolicy]]):
         self._env = env
         self._policies = policy if isinstance(policy, list) else [policy]
-
-        self.action_history = []
-        self.reward_history = []
+        self.history = History(self._env, self._policies)
 
     def train(self, n_episodes: int) -> list[BanditPolicy]:
         _ = self._env.reset()
@@ -25,33 +24,5 @@ class BanditTrainer:
                 rewards.append(reward)
                 p.update(action, reward)
 
-            self.action_history.append(actions)
-            self.reward_history.append(rewards)
+            self.history.update(actions, rewards)
         return self._policies
-
-    def display_history(self) -> None:
-        n = len(self.reward_history)
-        policies_avg_reward = np.cumsum(self.reward_history, axis=0) / np.array([np.arange(1, n + 1)]).T
-        print(policies_avg_reward.shape)
-
-        plt.subplot(1, 2, 1)
-        plt.xlabel("Steps")
-        plt.ylabel("Average reward")
-        for p, avg_reward in zip(self._policies, policies_avg_reward.T):
-            plt.plot(range(n), avg_reward, label=p.name)
-
-        distr = self._env.distributions
-        optimal_action = max(range(len(distr)), key=lambda d: distr[d].mean)
-        optimal_indicator = np.array(self.action_history) == optimal_action
-        pr_optimal_action = (
-            np.cumsum(optimal_indicator, axis=0) / np.array([np.arange(1, n + 1)]).T
-        )
-
-        plt.subplot(1, 2, 2)
-        plt.xlabel("Steps")
-        plt.ylabel("% Optimal action")
-        for p, optimal_action_arr in zip(self._policies, pr_optimal_action.T):
-            plt.plot(range(n), optimal_action_arr, label=p.name)
-
-        plt.legend()
-        plt.show()
--- src/sandbox/main.py
+++ src/sandbox/main.py
@@ -10,22 +10,22 @@ import numpy as np
 
 
 def main():
-    # np.random.seed(113)
+    np.random.seed(2)
     n_bandits = 10
     n_episodes = 10000
     bandits = [NormalDistribution(mean, 1) for mean in np.random.normal(0, 1, n_bandits)]
     
     env = BanditEnv(bandits)
-    policy1 = EpsilonGreedy(n_bandits, eps=0.1, init_value=0, name="EpsilonGreedy")
+    policy1 = EpsilonGreedy(n_bandits, eps=0.1, name="EpsilonGreedy")
     policy2 = UCB(n_bandits, init_value=0)
     policy3 = EpsilonGreedy(n_bandits, eps=0.1, init_value=5, name="EpsilonGreedy optimistic")
-    policy4 = ThompsonSampling(n_bandits, init_value=0)
+    policy4 = ThompsonSampling(n_bandits)
 
 
     trainer = BanditTrainer(env, [policy1, policy2, policy3, policy4])
     _ = trainer.train(n_episodes)
 
-    trainer.display_history()
+    trainer.history.display()
 
 
 if __name__ == '__main__':
