c87ccbd562620a61885285199199537e79ae3143
hankaj <hanna.jarlaczynska@gmail.com>
2022-05-05 11:32:07 +0200

    Deal with invalid moves

--- src/sandbox/enviroments/twenty_forty_eight/game/state.py
+++ src/sandbox/enviroments/twenty_forty_eight/game/state.py
@@ -1,4 +1,4 @@
-from base.state import State
+from sandbox.enviroments.twenty_forty_eight.base.state import State
 from dataclasses import dataclass
 from typing import Tuple, cast
 from numpy.typing import NDArray
