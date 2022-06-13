32bde8e1110317619d6f41306ef07bd67e1b1dab
Mateusz Slazynski <mateusz.slazynski@gmail.com>
2022-06-13 19:55:57 +0200

    Removes diagonal moves from the grid pathfinding environment

--- src/sandbox/enviroments/grid_pathfinding/problem/grid_move.py
+++ src/sandbox/enviroments/grid_pathfinding/problem/grid_move.py
@@ -4,26 +4,12 @@ from typing import Sequence, Tuple, Set
 
 
 class GridMove(Enum):
-    __order__ = 'N S W E NW NE SW SE'
+    __order__ = 'N E S W'
     N = (-1, 0)
+    E = (0, 1)
     S = (1, 0)
     W = (0, -1)
-    E = (0, 1)
-    NW = (-1, -1)
-    NE = (-1, 1)
-    SW = (1, -1)
-    SE = (1, 1)
-
-    @staticmethod
-    def diagonal_moves() -> Set[GridMove]:
-        return {GridMove.NW, GridMove.NE, GridMove.SW, GridMove.SE}
-
-    def involved_moves(self) -> Sequence[GridMove]:
-        if self not in GridMove.diagonal_moves():
-            return [self]
-        shift_l = (0, self.value[1])
-        shift_r = (self.value[0], 0)
-        return [self, GridMove(shift_l), GridMove(shift_r)]
+    
 
     def __str__(self) -> str:
         return self.name
