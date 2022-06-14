2029fa546791a260af3f5402becb31ff3e624990
Mateusz Slazynski <mateusz.slazynski@gmail.com>
2022-06-14 02:38:19 +0200

    Makes tabular tests interesting!

--- src/tasks/2-tabular-methods.py
+++ src/tasks/2-tabular-methods.py
@@ -1,6 +1,10 @@
 import logging
 import random
+from sandbox.action_selection_rules.epsilon_greedy_with_epsilon_decay import EpsilonGreedyActionSelectionWithDecayEpsilon
 from sandbox.action_selection_rules.greedy import GreedyActionSelection
+from sandbox.algorithms.algorithm import Algorithm
+from sandbox.algorithms.dyna.dynaq import DynaQ
+from sandbox.algorithms.dyna.dynaq_plus import DynaQPlus
 from sandbox.algorithms.q_learning.qlearning import QLearning
 from sandbox.algorithms.td_zero.td_zero import TDZero
 from sandbox.enviroments.multi_armed_bandit.env import NormalDistribution
@@ -16,12 +20,23 @@ from sandbox.algorithms.bandits_algorithm.bandits_algorithm import BanditsAlgori
 import sandbox.enviroments.grid_pathfinding as gp
 from pathlib import Path
 
+
+def _plot_name(algorithm: Algorithm) -> str:
+    match algorithm:
+        case QLearning():
+            return f"QLearning(alpha={algorithm._alpha})"        
+        case DynaQ():
+            return f"DynaQ(alpha={algorithm._alpha}, n={algorithm._planning_steps})"
+        case DynaQPlus():
+            return f"DynaQPlus(alpha={algorithm._alpha}, n={algorithm._planning_steps}, kappa={algorithm._kappa})"
+
+
 def grid_pathinding_benchmark():
     cmp = Comparator(
         algorithms=[
-            QLearning(.01, 1, EpsilonGreedyActionSelection(.1)),
-            QLearning(.05, 1, EpsilonGreedyActionSelection(.1)),
             QLearning(.1, 1, EpsilonGreedyActionSelection(.1)),
+            DynaQ(.1, 1, 50, EpsilonGreedyActionSelection(.1)),
+            DynaQPlus(.1, 1, 50, 0.001, EpsilonGreedyActionSelection(.1))
             ],
         envs=[
             DiscreteEnvironment(gym.make(
@@ -34,12 +49,31 @@ def grid_pathinding_benchmark():
             "custom/gridpathfinding-v0",
             file=f"{Path(gp.__file__).parent}/benchmarks/25.txt")),
         ],
-        get_label=lambda algo: f"{algo._alpha}", n_episodes=10000
+        get_label=_plot_name, n_episodes=1000
     )
-    cmp.run(list(PlotType))
+    cmp.run([PlotType.CumulatedReward])
+
+def dynamic_pathinding_benchmark():
+    n_episodes = 2000
+    cmp = Comparator(
+        algorithms=[
+            DynaQPlus(.1, 1, 50, 0.01, EpsilonGreedyActionSelectionWithDecayEpsilon(0.99, 0.99)),
+            DynaQ(.1, 1, 50, EpsilonGreedyActionSelectionWithDecayEpsilon(0.99, 0.99)),
+            QLearning(.1, 1, EpsilonGreedyActionSelectionWithDecayEpsilon(0.99, 0.99)),
+            ],
+        envs=[
+            DiscreteEnvironment(gym.make(
+            "custom/gridpathfinding-v0",
+            file=f"{Path(gp.__file__).parent}/benchmarks/13_dynamic.txt",
+            open_after=int(n_episodes / 3)))
+        ],
+        get_label=_plot_name, n_episodes=n_episodes
+    )
+    cmp.run([PlotType.CumulatedReward])
 
 if __name__ == '__main__':
     # NOTE: change logging level to info if you don't want to see ansi renders of env
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s\n%(message)s')
     grid_pathinding_benchmark()
+    dynamic_pathinding_benchmark()
 
